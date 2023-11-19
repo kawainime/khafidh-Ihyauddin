97244b2b82dd63e8df6b9e35696aeb53adb0d7ec
KawaiNime Pro <147604824+kawainime@users.noreply.github.com>
2023-11-19 15:37:13 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,9 +7,9 @@ export default function Footer({}: Props) {
     <footer className=' px-container flex flex-col gap-10 border-t-1 py-10'>
       <div className=' flex flex-col justify-evenly gap-10 md:flex-row'>
         <div className='flex flex-col'>
-          <h2 className='text-primary-text text-xl'>Azizah Mukarromah</h2>
+          <h2 className='text-primary-text text-xl'>Khafldh Ihyauddin</h2>
           <h3 className='text-primary-text text-medium font-light'>
-            Calon Anggota DPRD Kabupaten Ciamis
+            Calon Anggota DPRD Kabupaten Lamongan
           </h3>
         </div>
         <div className='flex flex-col gap-3'>
@@ -24,15 +24,15 @@ export default function Footer({}: Props) {
         <div className='flex flex-col gap-3'>
           <h2 className='text-primary-text text-xl'>Information</h2>
           <div className='text-primary-text flex flex-col gap-1 font-light'>
-            <span>Banjaranyar - Ciamis</span>
-            <span>Jawa Barat - Indonesia</span>
-            <span>+62-8522-1609-712</span>
-            <span>azizahmukarromah@gmail.com</span>
+            <span>Lamongan - Lamongan</span>
+            <span>Jawa Timur - Indonesia</span>
+            <span>+62-xxx-xxx</span>
+            <span>khafldhihyauddin@gmail.com</span>
           </div>
         </div>
       </div>
       <p className='border-t-1 pt-10 text-center text-sm'>
-        © 2023 Azizah Mukarromah. All rights reserved
+        © 2023 Khafldh Ihyauddln. All rights reserved
       </p>
     </footer>
   );
