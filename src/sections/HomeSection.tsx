0e025f9d51a7e405e8124558ba18fa5e5141c4f7
KawaiNime Pro <147604824+kawainime@users.noreply.github.com>
2023-11-23 23:00:16 +0700

    Update HomeSection.tsx

--- src/sections/HomeSection.tsx
+++ src/sections/HomeSection.tsx
@@ -33,7 +33,7 @@ export default function HomeSection({}: Props) {
               Khafidh Ihyauddin
             </p>
             <Image
-              src='/tentang-saya-1.png'
+              src='/verify.png'
               alt='Khafidh Ihyauddin'
               className='h-6 w-6 object-cover'
             />
